7b842cba55c41b698ef3039081a5feb06af11e34
TascuNicoleta <parlogeanicoleta@yahoo.com>
2026-08-01 08:56:26 +0300

    create project viora

---
+++ Viora/Viora/VioraApp.swift
@@ -0,0 +1,32 @@
+//
+//  VioraApp.swift
+//  Viora
+//
+//  Created by tascu nicoleta on 01/08/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct VioraApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
